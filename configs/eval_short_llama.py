29d41b9c8468c57840eea34e7d232233b9ac4398
Naman Tuli <namantuli2000@gmail.com>
2024-11-09 23:42:31 -0500

    updated script to check init

--- configs/eval_short_llama.py
+++ configs/eval_short_llama.py
@@ -5,17 +5,20 @@ with read_base():
     from opencompass.configs.datasets.demo.demo_gsm8k_chat_gen import gsm8k_datasets
     from opencompass.configs.datasets.demo.demo_math_chat_gen import math_datasets
 
-
-datasets = gsm8k_datasets + math_datasets
-
+datasets = gsm8k_datasets
 models = [
     dict(
         type=HuggingFaceCausalLM,
         abbr='short-llama',
         path='namannn/short-llama',
         tokenizer_path='namannn/short-llama',
-        model_kwargs=dict(device_map='auto'),
-        max_seq_len=2048,
+        model_kwargs=dict(
+            device_map='auto',
+            trust_remote_code=True,
+            load_in_8bit=False,  # Set to True if using 8-bit quantization
+            revision='main',  # Specify the correct revision/branch
+        ),
+        max_seq_len=1024,
         batch_size=4,
         run_cfg=dict(num_gpus=1),
     )
